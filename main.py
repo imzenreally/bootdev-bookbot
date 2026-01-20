dc85772b1cc3ef35acc76c298b8907383fe12f76
Tyler Vaughn <tyler.vaughn@salesforce.com>
2026-01-20 15:46:34 -0800

    updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, get_letter_count
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -8,6 +8,8 @@ def get_book_text(file_path):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_word_count(book_text)
+    letter_count = get_letter_count(book_text)
     print(f"Found {word_count} total words")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,8 @@
 def get_word_count(book_text):
     words = book_text.split()
     return len(words)
+
+def get_letter_count(book_text):
+    words = book_text.split()
+    letters = words.split()
+    print(letters)
